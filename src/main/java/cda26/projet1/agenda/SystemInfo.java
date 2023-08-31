82c6d406a4f4eb3b3f991ab411832e09bea8b7c9
Shaddowr88 <loicsunve@gmail.com>
2023-08-31 12:03:47 +0200

    début du projet

---
+++ src/main/java/cda26/projet1/agenda/SystemInfo.java
@@ -0,0 +1,13 @@
+package cda26.projet1.agenda;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module cda26.projet1.agenda {
+    requires javafx.controls;
+    exports cda26.projet1.agenda;
+}
