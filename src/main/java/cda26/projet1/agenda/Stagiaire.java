dae0ddd8fe52d806e4c279f033705a1d66b595cb
Bival2 <biaz2009@live.fr>
2023-09-01 16:03:05 +0200

    mon projet 1

---
+++ src/main/java/cda26/projet1/agenda/Stagiaire.java
@@ -0,0 +1,57 @@
+package cda26.projet1.agenda;
+
+public class Stagiaire {
+	// attributs
+	
+	public String nom;
+	public String prenom;
+	public int departement;
+	public String promotion;
+	public int annee;
+	
+	//contructeur
+	
+	public Stagiaire(String nom, String prenom, int departement, String promotion, int annee) {
+		
+		this.nom = nom;
+		this.prenom = prenom;
+		this.departement = departement;
+		this.promotion = promotion;
+		this.annee = annee;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getDepartement() {
+		return departement;
+	}
+	public void setDepartement(int departement) {
+		this.departement = departement;
+	}
+	public String getPromotion() {
+		return promotion;
+	}
+	public void setPromotion(String promotion) {
+		this.promotion = promotion;
+	}
+	public int getAnnee() {
+		return annee;
+	}
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+	
+	
+	
+	
+
+}
