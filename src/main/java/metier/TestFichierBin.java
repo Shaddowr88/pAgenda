531f8c03047d1a3d46c680fa7c93e3f27e688b6c
Shaddowr88 <loicsunve@gmail.com>
2023-09-07 10:44:28 +0200

    l

--- src/main/java/metier/TestFichierBin.java
+++ src/main/java/metier/TestFichierBin.java
@@ -8,26 +8,19 @@ import model.Stagiaire;
 
 public class TestFichierBin {
 
-	public static void main(String[] args) throws IOException {
+public static void main(String[] args) throws IOException {
 		
-		
-		EcritureFichierBin ecritureFichierBin = new EcritureFichierBin();
 		LectureFichier lectureFichier = new LectureFichier() ;
 		
-		//ecritureFichierBin.ecrireArbreDansFichierBin(0, test, test2); 
-		
 		//Attention, supprimer fichier bin des tests precedents avant de lancer ce main
-		//ecritureFichierBin.creerFichierBinaire(lectureFichier.lectureFichierDon().getPremierNoeud());
+
+		ArbreBinaire arbreAnnuaire = new ArbreBinaire() ;
 		
-		//ecritureFichierBin.ajouterStagiaireDansFichierBin(0, lectureFichier.lectureFichierDon().getPremierNoeud(), test2); 
+		lectureFichier.lectureFichierDon(arbreAnnuaire);
 		
-		ArbreBinaire arbreTest = lectureFichier.lectureFichierDon() ;
+		arbreAnnuaire.testLecture();
 		
-		System.out.println(arbreTest); //test affichage arbre lu en GND
-		System.out.println("Taille de l'arbre : " + arbreTest.tailleArbre());
-		System.out.println("Hauteur de l'arbre : " + arbreTest.hauteurArbre());
 		
-		ecritureFichierBin.creerArbreFichierBin(arbreTest, arbreTest.getPremierNoeud());
 	}
 	
 	
